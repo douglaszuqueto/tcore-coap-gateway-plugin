3ae8d747a8dfcb510a69187ebbe6c1013140c3e3
douglaszuqueto <douglas.zuqueto@gmail.com>
2022-10-13 06:16:50 -0300

    Update plugin icon

--- src/index.js
+++ src/index.js
@@ -1,18 +1,51 @@
-const { ServiceModule } = require("@tago-io/tcore-sdk");
+const { ServiceModule, pluginStorage } = require("@tago-io/tcore-sdk");
+const { spawn } = require("child_process");
+
+let child = null;
 
 const service = new ServiceModule({
   id: "tcore-coap-gateway-plugin",
   name: "CoAP Gateway Service",
+  configs: [
+    {
+      type: "number",
+      field: "port",
+      name: "Port",
+      icon: "cog",
+      required: true,
+      defaultValue: 5683,
+    },
+  ],
 });
 
-// `onLoad` is used to run your code.
-// This function will be called once when your plugin gets loaded.
-service.onLoad = async () => {
+service.onLoad = async (userValues) => {
   console.log("OnLoad Plugin");
+
+  //
+  // CoAP Service
+  //
+
+  child = spawn(__dirname + "/coap", {
+    env: {
+      COAP_PORT: userValues.port,
+    },
+  });
+
+  child.stdout.on("data", (data) => {
+    console.log(`${data}`);
+  });
+
+  child.on("exit", function (code, signal) {
+    console.log(
+      "child process exited with " + `code ${code} and signal ${signal}`
+    );
+  });
 };
 
 // `onDestroy` is used to clean up your code.
 // This function will never be called before `onLoad`.
 service.onDestroy = async () => {
   console.log("onDestroy Plugin");
+
+  child.kill("SIGINT");
 };
